8c2f32b15d787f90073c4c79fedd065b47c02950
Hongmin Park <park.hongmin@gmail.com>
2014-08-26 10:13:36 +0900

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,6 @@ class Pin < ActiveRecord::Base
 	has_many :comments
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	do_not_validate_attachment_file_type :image
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140824151321) do
+ActiveRecord::Schema.define(version: 20140825021323) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20140824151321) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
