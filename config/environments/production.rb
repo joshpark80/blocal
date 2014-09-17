8f559319ccf13b84c3b19ab2f31e9d565d83577e
Hongmin Park <park.hongmin@gmail.com>
2014-09-17 12:11:39 +0900

    before heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Pinteresting::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Required for Heroku
-  config.action_mailer.default_url_options = { :host => 'http://blocal-pinteresting.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'http://blocal.herokuapp.com/' }
   # Sets Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
   :storage => :s3,
