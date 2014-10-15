437a65bc08b602681c0bbb13d1c5ee907e1c65c0
Hongmin Park <park.hongmin@gmail.com>
2014-10-16 03:25:51 +0900

    avatar removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :pins 
-  has_attached_file :image, :styles => { :medium => "100x100#", :thumb => "50x50#" }, 
-  :default_url => "https://s3.amazonaws.com/b_local/users/images/default/:style/default_avatar.jpg"
+  has_attached_file :image, :styles => { :medium => "100x100#", :thumb => "50x50#" }
   do_not_validate_attachment_file_type :image
 
   has_many :favorite_pins, foreign_key:"favorited_id"  
